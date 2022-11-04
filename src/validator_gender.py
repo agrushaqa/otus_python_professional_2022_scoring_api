38ba97ae23ce455d8e354196c284f0d6ef08dd5f
Artem Grusha <grusha_av@inbox.ru>
2022-11-04 18:12:42 +0300

    add requirements.txt and small fix validator gender

--- src/validator_gender.py
+++ src/validator_gender.py
@@ -1,9 +1,10 @@
 class ValidatorGender:
     def __init__(self, message=None):
         self.value = message
+        if not isinstance(self.value, int):
+            raise ValueError(f"{self.value} type is {type(self.value)}")
         if self.value < 0:
             raise ValueError(f"{self.value} less then 0")
         if self.value > 2:
             raise ValueError(f"{self.value} more then 2")
-        if not isinstance(self.value, int):
-            raise ValueError(f"{self.value} type is {type(self.value)}")
+
